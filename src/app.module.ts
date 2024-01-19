babf8fb73c202d5029ef841e73ad6b752c051ee0
Bruno Bastos <brunobastos@edu.unifor.br>
2024-01-19 11:28:58 -0300

    adicionando o banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { CategoryModule } from './category/category.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://postgres:abc123@localhost:5432/devshop',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql'
